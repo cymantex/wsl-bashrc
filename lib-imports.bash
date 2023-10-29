7c0659b7fc86aee0358408f1bd44176dc2ecd7ae
Simon Eriksson <simon.eriksson@entiros.se>
2023-10-29 14:26:48 +0100

    Import misc-utils before git-utils

--- lib-imports.bash
+++ lib-imports.bash
@@ -17,8 +17,8 @@ export BASHRC_INSTALL="$WSL_BASHRC/install"
 # Utilities for working with our components through maven (all commands starts with "mvn")
 . "$BASHRC_LIB/mvn-utils.bash"
 
-# Utilities for working with git and Gerrit (all commands starts with "git")
-. "$BASHRC_LIB/git-utils.bash"
-
 # A collection of various other functions/aliases that have been proven useful
 . "$BASHRC_LIB/misc-utils.bash"
+
+# Utilities for working with git and Gerrit (all commands starts with "git")
+. "$BASHRC_LIB/git-utils.bash"
